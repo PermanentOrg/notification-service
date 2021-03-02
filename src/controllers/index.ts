6ea2a03d95d5414561d23bc476316e70649ea57c
Mithuna Krishna <mkrishna@permanent.org>
2021-03-02 15:21:48 -0600

    Create a Notification in database
    create a table through migration.
    New endpoint to insert a row in notifications table.
    
    Issue #11 Add initial API for notifications
    
    Co-authored-by: Jason Owen <jasonaowen@opentechstrategies.com>

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,2 @@
 export { healthController } from './health.controller';
+export { notificationController } from './notification.controller';
---
+++ src/services/notification.service.ts
@@ -0,0 +1,22 @@
+import { db } from './database';
+
+interface Notification{
+  toUserId: number;
+  notificationType: string;
+}
+
+interface InsertNotificationResult{
+  notification_id: number;
+}
+
+const createNotification = async (notification: Notification): Promise<number> => {
+  const result = await db.sql<InsertNotificationResult>('insert-notification', {
+    to_user_id: notification.toUserId,
+    notification_type: notification.notificationType,
+  });
+  return result.rows[0].notification_id;
+};
+
+export const notificationService = {
+  createNotification,
+};
