f69d1f9c4476a104cac09bcac89b3caaa433b735
Mithuna Krishna <mkrishna@permanent.org>
2021-04-07 17:13:04 -0500

    Fixing eslint errors

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -9,15 +9,15 @@ interface InsertNotificationResult{
   notification_id: number;
 }
 
-const createNotification = async (notification: Notification): Promise<Object> => {
+const createNotification = async (notification: Notification): Promise<unknown> => {
   const result = await db.sql<InsertNotificationResult>('insert-notification', {
     to_user_id: notification.toUserId,
     notification_type: notification.notificationType,
   });
   const jsonResult = {
     notificationId: result.rows[0].notification_id,
-    message: "Successfully created a notification"
-  }
+    message: 'Successfully created a notification',
+  };
   return jsonResult;
 };
 
