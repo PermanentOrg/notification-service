4402ff0b1c7549f933b79c0485249b65c865da89
Mithuna Krishna <mkrishna@permanent.org>
2021-04-08 11:49:08 -0500

    Adding an interface and taking into account error condition
    An interface to return the notification ID and a success message. In
    case of error this will return notification ID as 0 (hoping that is okay
    suggestions welcome) and the error message.

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -9,16 +9,31 @@ interface InsertNotificationResult{
   notification_id: number;
 }
 
-const createNotification = async (notification: Notification): Promise<unknown> => {
-  const result = await db.sql<InsertNotificationResult>('insert-notification', {
-    to_user_id: notification.toUserId,
-    notification_type: notification.notificationType,
-  });
-  const jsonResult = {
-    notificationId: result.rows[0].notification_id,
-    message: 'Successfully created a notification',
-  };
-  return jsonResult;
+interface NotificationResponse{
+  notificationId: number;
+  message: string;
+}
+
+const createNotification = async (notification: Notification): Promise<NotificationResponse> => {
+  try {
+    const result = await db.sql<InsertNotificationResult>('insert-notification', {
+      to_user_id: notification.toUserId,
+      notification_type: notification.notificationType,
+    });
+    return {
+      notificationId: result.rows[0].notification_id,
+      message: 'Successfully created a notification',
+    };
+  } catch (err: unknown) {
+    let errmessage = 'unknown error';
+    if (err instanceof Error) {
+      errmessage = err.message;
+    }
+    return {
+      notificationId: 0,
+      message: errmessage,
+    };
+  }
 };
 
 export const notificationService = {
