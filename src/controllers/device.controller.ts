0befef2a72c119e0315a6583dbae0cb4af7fb555
Mithuna Krishna <mkrishna@permanent.org>
2021-05-27 11:50:01 -0500

    Delete a token
    This code will remove input token from the database.
    
    I am removing all entries of the token from database. 2 accounts
    cannot be logged in at the same time and don't expect multiple
    entries of token when this change will be called as a user logs out.

--- src/controllers/device.controller.ts
+++ src/controllers/device.controller.ts
@@ -6,6 +6,11 @@ import type {
 import { deviceService } from '../services';
 import { validateCreateDeviceParams } from '../validators';
 
+interface Devices{
+  userId: number;
+  token: string;
+}
+
 const addDevice: Handler = (
   req: Request,
   res: Response,
@@ -21,7 +26,23 @@ const addDevice: Handler = (
       }));
   }
 };
-
+const deleteDevice: Handler = (
+  req: Request,
+  res: Response,
+): void => {
+  const validation = validateCreateDeviceParams(req.body);
+  if (validation.error) {
+    res.status(400).json({ error: validation.error });
+  } else {
+    const device = req.body as Devices;
+    deviceService.removeDeviceToken(device.token)
+      .then(() => res.json('Device is successfully removed.'))
+      .catch((err: unknown) => res.status(500).json({
+        error: err,
+      }));
+  }
+};
 export const deviceController = {
   addDevice,
+  deleteDevice,
 };
