3233475c8870bfe389876ad477d525e470008679
Dan Schultz <slifty@gmail.com>
2025-07-09 14:17:55 -0400

    Enable `love` lint rules
    Issue #121 Update to ESLint 9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,15 @@ import typescriptEslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 import globals from "globals";
 import jest from "eslint-plugin-jest";
-import pluginImport from "eslint-plugin-import";
 import js from "@eslint/js";
+import love from "eslint-config-love";
 
 export default defineConfig([
 	js.configs.recommended,
 	typescriptEslint.configs.eslintRecommended,
 	typescriptEslint.configs.recommendedTypeChecked,
 	typescriptEslint.configs.strict,
+	love,
 	prettier,
 	{
 		languageOptions: {
@@ -22,8 +23,6 @@ export default defineConfig([
 				project: "./tsconfig.json",
 			},
 		},
-		plugins: { import: pluginImport },
-
 		rules: {
 			"import/prefer-default-export": "off",
 			"import/no-default-export": "error",
@@ -35,6 +34,18 @@ export default defineConfig([
 					caughtErrorsIgnorePattern: "^_",
 				},
 			],
+
+			// These are rules enabled by `love` which the project currently violates.
+			// We will re-enable these alongside the necessary fixes over time.
+			"@typescript-eslint/strict-boolean-expressions": "off",
+			"@typescript-eslint/no-magic-numbers": "off",
+			"@typescript-eslint/no-unsafe-type-assertion": "off",
+			"max-nested-callbacks": "off",
+			"eslint-comments/disable-enable-pair": "off",
+			"@typescript-eslint/return-await": "off",
+			"@typescript-eslint/use-unknown-in-catch-callback-variable": "off",
+			"import/enforce-node-protocol-usage": "off",
+			"@typescript-eslint/no-deprecated": "off",
 		},
 	},
 	{
